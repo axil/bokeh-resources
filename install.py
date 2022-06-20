f8fd5dce838953910023536d3d9242e12ff01e26
Lev Maximov <lev.maximov@gmail.com>
2022-06-21 01:29:35 +0700

    linux support

--- install.py
+++ install.py
@@ -1,8 +1,12 @@
 import os
+import sys
 from pathlib import Path
 import bokeh
 from notebook.nbextensions import install_nbextension
 
 alias = Path(install_nbextension('bokeh_resources')) / 'static'
 target = Path(bokeh.__file__).parent / 'server' / 'static'
-os.system(f'mklink /j {alias} {target}')
+if sys.platform == 'win32':
+    os.system(f'mklink /j {alias} {target}')
+else:
+    alias.symlink_to(target)
