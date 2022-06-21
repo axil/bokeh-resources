ac64e32a23b5c0c44710a9e8776ac6466a6c1d83
Lev Maximov <lev.maximov@gmail.com>
2022-06-21 18:23:23 +0700

    improved the install script

--- bokeh_resources/install.py
+++ bokeh_resources/install.py
@@ -4,9 +4,15 @@ from pathlib import Path
 import bokeh
 from notebook.nbextensions import install_nbextension
 
+os.chdir(Path(__file__).parent)
 alias = Path(install_nbextension('bokeh_resources')) / 'static'
 target = Path(bokeh.__file__).parent / 'server' / 'static'
-if sys.platform == 'win32':
-    os.system(f'mklink /j {alias} {target}')
+print(f'Creating symlink {alias} pointing to {target}')
+if alias.exists():
+    print('Symlink already exists, exiting')
 else:
-    alias.symlink_to(target)
+    if sys.platform == 'win32':
+        os.system(f'mklink /j {alias} {target}')
+    else:
+        alias.symlink_to(target)
+    print('Symlink created')
